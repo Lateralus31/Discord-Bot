601daab9fcf609aa4ca9a90cf28c822dbe928ce1
root <root@nodejs-discordbot.localdomain>
2016-12-25 11:10:05 +0000

    Bot can now join voice channels

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 //Reply in chat room back to specific message
-bot.on('message', (message) =>
-{
+bot.on('message', (message) => {
   if(message.content == "jake")
   {
     message.channel.sendMessage('is gay');
@@ -11,11 +10,27 @@ bot.on('message', (message) =>
 });
 
 //Reply in PM to specific message
-bot.on('message', (message) =>
-{
+bot.on('message', (message) => {
   if(message.content == "zane")
   {
     message.author.sendMessage("has a sick emoji!");
+  }
+});
+
+bot.on('message', message => {
+  if (message.content.startsWith('++play')) {
+    const voiceChannel = message.member.voiceChannel;
+    if (!voiceChannel) {
+      return message.reply(`Please be in a voice channel first!`);
+    }
+    voiceChannel.join()
+      .then(connnection => {
+        let stream = yt("https://www.youtube.com/watch?v=dQw4w9WgXcQ", {audioonly: true});
+        const dispatcher = connnection.playStream(stream);
+        dispatcher.on('end', () => {
+          voiceChannel.leave();
+        });
+      });
   }
 });
 
